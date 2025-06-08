d5be9f7a673a856b1a7c41473a3dadd090bcf3ac
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-06-08 23:07:51 +0530

    Project finalized

--- system/src/main/java/com/system/controller/DepartmentController.java
+++ system/src/main/java/com/system/controller/DepartmentController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +18,49 @@ import com.system.model.Department;
 import com.system.service.DepartmentService;
 
 @RestController
-@RequestMapping("/deparment")
+@RequestMapping("/department")
 public class DepartmentController {
 	@Autowired
 	private DepartmentService service;
 
-	@GetMapping("/")
-	public ResponseEntity<List<Department>> getDeps() {
-		return new ResponseEntity<List<Department>>(service.findDeps(), HttpStatus.OK);
+	@GetMapping
+	public ResponseEntity<List<Department>> getDepts() {
+		return new ResponseEntity<List<Department>>(service.getDepts(), HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Department> getDept(@PathVariable("id") int id) {
+		if (service.getDept(id) == null) {
+			return new ResponseEntity<Department>(service.getDept(id), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Department>(service.getDept(id), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<String> addDept(@RequestBody Department department) {
+		String result = service.addDept(department);
+
+		if (result.equals("Department already exists.")) {
+			return new ResponseEntity<>(result, HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> updateDept(@PathVariable("id") int id, @RequestBody Department department) {
+		String result = service.updateDept(id, department);
+		if (result.equals("Department updated.")) {
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteDept(@PathVariable("id") int id) {
+		String result = service.deleteDept(id);
+		if (result.equals("Department deleted.")) {
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		}
+		return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
 	}
 }
--- system/src/main/java/com/system/service/DepartmentService.java
+++ system/src/main/java/com/system/service/DepartmentService.java
@@ -1,6 +1,7 @@
 package com.system.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,38 @@ public class DepartmentService {
 	@Autowired
 	private DepartmentRepo repo;
 
-	public List<Department> findDeps() {
+	public List<Department> getDepts() {
 		return repo.findAll();
+	}
+
+	public Department getDept(int id) {
+		if (repo.findById(id).isEmpty()) {
+			return null;
+		}
+		return repo.findById(id).get();
+	}
+
+	public String addDept(Department department) {
+		if (repo.findById(department.getId()).isPresent()) {
+			return "Department already exists.";
+		}
+		repo.save(department);
+		return "New department added.";
+	}
+
+	public String updateDept(int id, Department department) {
+		if (repo.existsById(id)) {
+			repo.save(department);
+			return "Department updated.";
+		}
+		return "Department not exists.";
+	}
+
+	public String deleteDept(int id) {
+		if (repo.existsById(id)) {
+			repo.deleteById(id);
+			return "Department deleted.";
+		}
+		return "Department not exists.";
 	}
 }
